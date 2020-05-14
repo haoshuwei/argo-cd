e462e6c02d26df5ee87a74cf97783513a2efb60e
指北 <jonas.ljq@alibaba-inc.com>
2020-05-14 10:56:49 +0800

    add alibaba-cloud-sdk-go to vendor

--- vendor/github.com/json-iterator/go/extension_tests/decoder_test.go
+++ vendor/github.com/json-iterator/go/extension_tests/decoder_test.go
@@ -1,6 +1,8 @@
 package test
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/json-iterator/go"
 	"github.com/stretchr/testify/require"
 	"strconv"
@@ -44,6 +46,38 @@ func Test_customize_byte_array_encoder(t *testing.T) {
 	str, err := jsoniter.MarshalToString(val)
 	should.Nil(err)
 	should.Equal(`"abc"`, str)
+}
+
+type CustomEncoderAttachmentTestStruct struct {
+	Value int32 `json:"value"`
+}
+
+type CustomEncoderAttachmentTestStructEncoder struct {}
+
+func (c *CustomEncoderAttachmentTestStructEncoder) Encode(ptr unsafe.Pointer, stream *jsoniter.Stream) {
+	attachVal, ok := stream.Attachment.(int)
+	stream.WriteRaw(`"`)
+	stream.WriteRaw(fmt.Sprintf("%t %d", ok, attachVal))
+	stream.WriteRaw(`"`)
+}
+
+func (c *CustomEncoderAttachmentTestStructEncoder) IsEmpty(ptr unsafe.Pointer) bool {
+	return false
+}
+
+func Test_custom_encoder_attachment(t *testing.T) {
+
+	jsoniter.RegisterTypeEncoder("test.CustomEncoderAttachmentTestStruct", &CustomEncoderAttachmentTestStructEncoder{})
+	expectedValue := 17
+	should := require.New(t)
+	buf := &bytes.Buffer{}
+	stream := jsoniter.NewStream(jsoniter.Config{SortMapKeys: true}.Froze(), buf, 4096)
+	stream.Attachment = expectedValue
+	val := map[string]CustomEncoderAttachmentTestStruct{"a": {}}
+	stream.WriteVal(val)
+	stream.Flush()
+	should.Nil(stream.Error)
+	should.Equal("{\"a\":\"true 17\"}", buf.String())
 }
 
 func Test_customize_field_decoder(t *testing.T) {
@@ -97,4 +131,93 @@ func Test_read_custom_interface(t *testing.T) {
 	err := jsoniter.UnmarshalFromString(`"hello"`, &val)
 	should.Nil(err)
 	should.Equal("hello", val.Hello())
+}
+
+const flow1 = `
+{"A":"hello"}
+{"A":"hello"}
+{"A":"hello"}
+{"A":"hello"}
+{"A":"hello"}`
+
+const flow2 = `
+{"A":"hello"}
+{"A":"hello"}
+{"A":"hello"}
+{"A":"hello"}
+{"A":"hello"}
+`
+
+type (
+	Type1 struct {
+		A string
+	}
+
+	Type2 struct {
+		A string
+	}
+)
+
+func (t *Type2) UnmarshalJSON(data []byte) error {
+	return nil
+}
+
+func (t *Type2) MarshalJSON() ([]byte, error) {
+	return nil, nil
+}
+
+func TestType1NoFinalLF(t *testing.T) {
+	reader := bytes.NewReader([]byte(flow1))
+	dec := jsoniter.NewDecoder(reader)
+
+	i := 0
+	for dec.More() {
+		data := &Type1{}
+		if err := dec.Decode(data); err != nil {
+			t.Errorf("at %v got %v", i, err)
+		}
+		i++
+	}
+}
+
+func TestType1FinalLF(t *testing.T) {
+	reader := bytes.NewReader([]byte(flow2))
+	dec := jsoniter.NewDecoder(reader)
+
+	i := 0
+	for dec.More() {
+		data := &Type1{}
+		if err := dec.Decode(data); err != nil {
+			t.Errorf("at %v got %v", i, err)
+		}
+		i++
+	}
+}
+
+func TestType2NoFinalLF(t *testing.T) {
+	reader := bytes.NewReader([]byte(flow1))
+	dec := jsoniter.NewDecoder(reader)
+
+	i := 0
+	for dec.More() {
+		data := &Type2{}
+		if err := dec.Decode(data); err != nil {
+			t.Errorf("at %v got %v", i, err)
+		}
+		i++
+	}
+}
+
+func TestType2FinalLF(t *testing.T) {
+	reader := bytes.NewReader([]byte(flow2))
+	dec := jsoniter.NewDecoder(reader)
+
+	i := 0
+	for dec.More() {
+		data := &Type2{}
+		if err := dec.Decode(data); err != nil {
+			t.Errorf("at %v got %v", i, err)
+		}
+		i++
+	}
 }
